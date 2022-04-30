76e987f9213967d6657dcfe302360fc87f883a48
Sebastian <sebastiansasaki@gmail.com>
2022-04-30 15:52:52 -0500

    My Events 75% done

---
+++ app/src/main/java/com/example/findevents/my_events.kt
@@ -0,0 +1,11 @@
+package com.example.findevents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class my_events : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_events)
+    }
+}
